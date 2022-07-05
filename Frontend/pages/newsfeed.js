5acfaed14334645c41efc2b2959a526d48cd049a
유혜선 <>
2022-07-06 00:22:48 +0900

    Feat. : 뉴스피드 footer UI 적용

--- Frontend/pages/newsfeed.js
+++ Frontend/pages/newsfeed.js
@@ -1,5 +1,43 @@
+import styles from "../styles/Home.module.scss";
+import Image from "next/image";
+import homeImg from "../public/img/menu_home.png"
+import profileImg from "../public/img/menu_profile.png"
+
 const NewsFeed = () => {
-  return;
+  return (
+
+    <footer className={styles.footer_button}>
+      <div
+        className={styles.footer_button_left}
+      >
+        <button className={styles.button_default}>
+          
+          {/* <div className={styles.circle}> */}
+            <Image
+              className={styles.circle_img}
+              src={homeImg}
+              width={35}
+              height={35} />Home
+          {/* </div> */}
+
+        </button>
+      </div>
+      <div
+        className={styles.footer_button_right}
+      >
+        <button className={styles.button_default}>
+          {/* <div className={styles.circle}> */}
+            <Image
+              className={styles.circle_img}
+              src={profileImg}
+              width={35}
+              height={35} />MyPage
+          {/* </div> */}
+
+        </button>
+      </div>
+    </footer>
+  );
 };
 
 export default NewsFeed;
